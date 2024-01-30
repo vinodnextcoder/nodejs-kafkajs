a2f28a42bbece9ee4cca407fe7f6826b51409842
vinod <vinodk2757@gmail.com>
2024-01-30 15:31:44 +0530

    kafka js

---
+++ index.js
@@ -0,0 +1,39 @@
+const { Kafka } = require('kafkajs')
+
+const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['localhost:9092'],
+})
+
+const producerkafka = async () => {
+    const producer = kafka.producer()
+
+    await producer.connect()
+    await producer.send({
+        topic: 'test-topic',
+        messages: [
+            { value: 'Hello KafkaJS user!' },
+        ],
+    })
+
+    await producer.disconnect()
+
+}
+//
+
+const consumerkaf = async () => {
+    const consumer = kafka.consumer({ groupId: 'test-group' })
+
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log({
+                value: message.value.toString(),
+            })
+        },
+    })
+}
+producerkafka()
+consumerkaf()
